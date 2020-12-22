2fa44b3d1603178fca68d4cc2f3b6e3527dd98d8
bugsounet <github@bugsounet.fr>
2020-12-22 14:44:00 +0100

    correct bug if no description // add samples

--- MMM-NewsFeed.js
+++ MMM-NewsFeed.js
@@ -81,6 +81,7 @@ Module.register("MMM-NewsFeed", {
       this.item = -1
       return this.DisplayNext()
     }
+    if (!this.RSS[this.item].description || this.RSS[this.item].description == "") return this.DisplayNext()
     if (this.item > this.RSS.length-1) this.item = 0
 
     var title = document.getElementById("NEWSFEED_TITLE")
